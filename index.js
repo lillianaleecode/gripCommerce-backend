444a1d4f90c27387a0f23a24b1bf9990059fb5f2
lillianaleecode <lilliana.lee@code.berlin>
2022-06-28 19:28:53 +0200

    created -env file to save secret keys

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config()
 
 //replaced the password to "ORACLE" and the name of the database ("cake_shop"). ROOT USER PWD IS "ORACLE" created from the DB access section.
+//created a secret key "MONGO_URL" in .env to protect strangers to access my mongoDB. 
 mongoose
-.connect("mongodb+srv://root:ORACLE@node.zn6hp.mongodb.net/cake_shop?retryWrites=true&w=majority")
+.connect(process.env.MONGO_URL)
 .then( ()=> console.log("DB Connection Succesful:)"))
 .catch((err) => {
     console.error(err);
