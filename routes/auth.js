334427c6725c10049dc2d96e425ff6ea50e6f801
lillianaleecode <lilliana.lee@code.berlin>
2022-07-13 13:00:05 +0200

    JWT Implementation. login api

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 //register
 //REGISTER
@@ -42,8 +43,15 @@ router.post('/login', async (req, res) => {
       originalPassword != req.body.password && 
       res.status(401).json("Wrong Password");
 
+      //to make the app more secure
+      const accessToken = jwt.sign({
+        id: user._id,
+        isAdmin: user.isAdmin,
+
+      }, process.env.JWT_SEC, {expiresIn: "7d"});
+
       const { password, ...others } = user._doc;
-      res.status(200).json(others)
+      res.status(200).json({...others, accessToken })
 
     } catch(err){
       res.status(500).json(err);
