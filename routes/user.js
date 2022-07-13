b41f985e66616cf3611af768fbea0c7e1ea2e3a3
lillianaleecode <lilliana.lee@code.berlin>
2022-07-13 15:44:19 +0200

    update the username. PUT Method. with User.findByIdAndUpdate()

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,30 @@
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 const router = require("express").Router();
-const {verifyToken, verifyTokenAndAuth} = require("./verifyToken");
+const {verifyTokenAndAuth} = require("./verifyToken");
 
-router.put("/:id", verifyTokenAndAuth, (req, res) =>{
-    
+//to update
+router.put("/:id", verifyTokenAndAuth, async (req, res) => {
+
+    //before updating, encript the password
+    if(req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SEC
+        ).toString();
+    }
+
+    //update:
+    try{
+        const updateUser = await User.findByIdAndUpdate(
+            req.params.id, { $set: req.body }, {new: true }
+            );
+        
+        res.status(200).json(updateUser);
+
+    } catch(err){
+        res.status(500).json(err);
+    }
 })
 
 module.exports = router
