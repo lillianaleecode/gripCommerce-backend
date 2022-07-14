6ab08a2328889110f5b01551f521b83bbccad9a7
lillianaleecode <lilliana.lee@code.berlin>
2022-07-14 17:20:15 +0200

    create product route. post request

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user")
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
 
 dotenv.config()
 
@@ -25,6 +26,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/product", productRoute);
 
 
 app.listen(PORT , () => console.log(`backend started on ${PORT}` ))
