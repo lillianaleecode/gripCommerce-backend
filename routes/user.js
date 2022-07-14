aa71ff0b99887cc1045ffb49c013a5429d13db92
lillianaleecode <lilliana.lee@code.berlin>
2022-07-14 13:15:21 +0200

    delete user router function

--- routes/user.js
+++ routes/user.js
@@ -27,4 +27,14 @@ router.put("/:id", verifyTokenAndAuth, async (req, res) => {
     }
 })
 
+//delete user
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("User is deleted.");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
+
 module.exports = router
