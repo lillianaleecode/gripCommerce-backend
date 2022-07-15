ca4493dbe184ff36519b56771e093ed7d55cc834
lillianaleecode <lilliana.lee@code.berlin>
2022-07-15 12:42:06 +0200

    order route. similar to products and cart route crud logic

---
+++ routes/order.js
@@ -0,0 +1,63 @@
+//order route is similar to products and cart routes. with the slight difference that users can have more than one order, and not only the admin can access the data.
+
+const router = require("express").Router();
+const Order = require("../models/Order");
+const {verifyToken, verifyTokenAndAuth, verifyTokenAndAdmin} = require("./verifyToken");
+
+//CREATE
+router.post("/", verifyToken, async (req, res) => {
+  const newOrder = new Order(req.body);
+
+  try {
+    const savedOrder = await newOrder.save();
+    res.status(200).json(savedOrder);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//UPDATE
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedOrder = await Order.findByIdAndUpdate(
+      req.params.id,{ $set: req.body, },{ new: true });
+
+    res.status(200).json(updatedOrder);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//DELETE
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await Order.findByIdAndDelete(req.params.id);
+    res.status(200).json("Order just deleted.");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET USER ORDERS
+router.get("/find/:userId", verifyTokenAndAuth, async (req, res) => {
+  try {
+    const orders = await Order.find({ userId: req.params.userId });
+    res.status(200).json(orders);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// //GET ALL
+
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const orders = await Order.find();
+    res.status(200).json(orders);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+module.exports = router;
